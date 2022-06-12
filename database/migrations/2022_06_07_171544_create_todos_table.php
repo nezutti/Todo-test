b71fe3e26bd4b42a79b9ab45f38490eb3228eca3
久保祐輔 <bokukuboyu@icloud.com>
2022-06-12 14:10:26 +0900

    submit

--- database/migrations/2022_06_07_171544_create_todos_table.php
+++ database/migrations/2022_06_07_171544_create_todos_table.php
@@ -15,7 +15,7 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->string("content",191)->nullable(false);
+            $table->string("content",20)->nullable(false);
             $table->timestamp("created_at")->useCurrent()->nullable();
             $table->timestamp("updated_at")->useCurrent()->nullable();
         });
